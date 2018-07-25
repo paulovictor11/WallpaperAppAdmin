ca0d5fbc89de45827260de822c6c4cb1dfb336c4
paulovictor11 <pauloalmeidajp652@gmail.com>
2018-07-24 21:41:47 -0300

    Versão 0.8

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+var config = {
+    apiKey: "AIzaSyBPmaxYBJK9GjriW4b14N_cOEuCTJ7va74",
+    authDomain: "wallpaperapp-491e2.firebaseapp.com",
+    databaseURL: "https://wallpaperapp-491e2.firebaseio.com",
+    projectId: "wallpaperapp-491e2",
+    storageBucket: "wallpaperapp-491e2.appspot.com",
+    messagingSenderId: "690442892897"
+};
+
+firebase.initializeApp(config);
+
+firebase.auth.Auth.Persistence.LOCAL;
+
+$("#btn-login").click(function () {
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    var result = firebase.auth().signInWithEmailAndPassword(email, password);
+    result.catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+});
+
+function switchView(view) {
+    $.get({
+        url: view,
+        cache: false
+    }).then(function (data){
+        $("#container").html(data);
+    });
+}
